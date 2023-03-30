30034888d622fb428ae18fae2293c042924076f9
Andrey Karimov <AndreyKarimov@gmail.com>
2023-03-30 22:31:31 +0300

    add module 71114

---
+++ 71114.py
@@ -0,0 +1,16 @@
+unicComment = {
+    'Дили' : set(),
+    'Били' : set(),
+    'Вили' : set(),
+}
+while True:
+    line = input()
+    if line == 'конец':
+        break
+    else:
+        name, name_comm = line.split(': ')
+        unicComment.get(name).add(name_comm)
+
+for key, value in sorted(unicComment.items(), key=lambda x: len(x[1]), reverse=True):
+    print(f'Количество уникальных комментаторов у {key} - {len(value)}')
+
